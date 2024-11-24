fa9790f5214560b9df18735b8afafc9286e698e8
Caleb Behayilu <calebbehayilu@gmail.com>
2024-11-24 13:54:48 +0300

    📦 NEW: Major Deploy.

---
+++ app/components/Photo_Display.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Photo_Display = () => {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+      <div className="grid gap-2">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="/img_1.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="/img_6.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="grid gap-2">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="/img_7.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="/img_4.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="grid gap-2">
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="/img_5.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="/img_8.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Photo_Display;
--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,43 @@
 import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
+import { TbBrandTelegram } from "react-icons/tb";
+import Photo_Display from "./components/Photo_Display";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid items-center justify-items-center  font-[family-name:var(--font-montserrat)] ">
+      <main className="flex flex-col row-start-1 p-20 items-center sm:items-start">
+        <div className="grid grid-cols-2 items-center">
+          <h1 className="font-bold text-4xl px-5 bg-gradient-to-r from-teal-200 to-teal-500 bg-clip-text text-transparent ">
+            Experience Harar's rich tradition and quality embroidery, weaving
+            heritage and elegance into every stitch.
+            <br />
+            <div className="text-center">
+              <p className="px-5 py-3 bg-gradient-to-r from-amber-200 to-yellow-500 bg-clip-text text-transparent">
+                Coming Soon
+              </p>
+            </div>
+          </h1>
+          {/* <img src="/harar_stitch_colored.png" alt="" /> */}
+          <Photo_Display />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-2 flex gap-6 flex-wrap items-center justify-center py-10">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.instagram.com/harar_stitch?igsh=dXlrZ2Nsd3E4eGhr&utm_source=qr"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          <FaInstagram size={25} />
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://t.me/HararStitch"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          <TbBrandTelegram size={25} />
         </a>
       </footer>
     </div>
